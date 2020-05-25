06685fa68d16e4e6616de6f0e3e360ba36716ccf
Ming-der Wang <ming@log4analytics.com>
2020-05-25 11:19:57 +0800

    ok for attack.sh

--- scripts/attackloop.sh
+++ scripts/attackloop.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+rly q unrelay
+rly tx rly d5 -d
 while true; do 
 for i in {1..3}
 do
+	echo "try $i times"
    ./loop.sh
 done
-	
+rly tx rly d5 -d
+echo "---flush------------------"
+rly q unrelayed d5
+echo "---------------------"
 done
